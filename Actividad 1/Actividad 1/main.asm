d8e3a901be83e6589b33895ef4167d34faff105f
itkg10 <142331492+itkg10@users.noreply.github.com>
2024-02-15 20:01:23 -0600

    main.asm
    Actualizado con los comandos correctos para 16 bits

--- Actividad 1/Actividad 1/main.asm
+++ Actividad 1/Actividad 1/main.asm
@@ -3,7 +3,7 @@
 ;
 ; Created: 14/02/2024 12:42:03 a. m.
 ; Author : Uriel Huerta, Axel Vallejo, Giovani Orozco
-;Programa para generar 4 diferentes seÒales con frecuencias distintas
+;Programa para generar 4 diferentes se√±ales con frecuencias distintas
 ;dependiendo de los estados de entrada
 
 .cseg	
@@ -13,49 +13,49 @@
 
 ;Main program
 	;Stack init
-	ldi temp,high(RAMEND) ;High byte
-	out SPH,temp
-	ldi temp,low(RAMEND) ;Low byte
-	out SPL,temp
+	lds temp,high(RAMEND) ;High byte
+	sts SPH,temp
+	lds temp,low(RAMEND) ;Low byte
+	sts SPL,temp
 
 
 ;Port configuration
-	ldi temp,0b00100000 ; PB5->output // input->PB4 y PB3 (por posicion de 0's y 1's) 
-	out DDRB,temp 
+	lds temp,0b00100000 ; PB5->output // input->PB4 y PB3 (por posicion de 0's y 1's) 
+	sts DDRB,temp 
 	sbi PORTB,PB4 ;activa la resistencia de pull-up en puerto 4
 	sbi PORTB,PB3 ;activa la resistencia de pull-up en puerto 3 
 ;PORTB coloca el puerto en modo escritura
-;En estos momentos ambos puertos estarÌan con un 1 lÛgico
+;En estos momentos ambos puertos estar√≠an con un 1 l√≥gico
 	
 ;Inicializar el TIMER1 de 16 bits Pagina 123 datasheet
-	ldi r17,0xFF ; Byte m·s alto de 0xFFFF
-	ldi r16,0xFF ; Byte m·s bajo de 0xFFFF
-	out TCNT1H,R17 
-	out TCNT1L,R16
+	lds r17,0xFF ; Byte m√°s alto de 0xFFFF
+	lds r16,0xFF ; Byte m√°s bajo de 0xFFFF
+	sts TCNT1H,R17 
+	sts TCNT1L,R16
 
-;Configurar el modo de operaciÛn del Timer1
-	ldi r17,0x00 ; Modo Normal
-	out TCCR1B,R17
+;Configurar el modo de operaci√≥n del Timer1
+	lds r17,0x00 ; Modo Normal
+	sts TCCR1B,R17
 
 ;Configurar el prescaler
-	ldi r17,0x06 ; Prescaler de 256
-	out TCCR1B,R17
+	lds r17,0x05 ; Prescaler de 1024
+	sts TCCR1B,R17
 
-;Habilitar la interrupciÛn del Timer1
-	ldi r17,0x01 ; Habilitar interrupciÛn TOIE1
-	out TIMSK1,R17
+;Habilitar la interrupci√≥n del Timer1
+	lds r17,0x01 ; Habilitar interrupci√≥n TOIE1
+	sts TIMSK1,R17
 
 ;Subrutinas
 entrada_00:
 	in temp,PB3
-	andi temp,0b00000000 ;verifica si est· en ceros
-	brne freq1 ;si est· en 00 salta a la frecuencia 1 
+	andi temp,0b00000000 ;verifica si est√° en ceros
+	brne freq1 ;si est√° en 00 salta a la frecuencia 1 
 	cbi PORTB,PB3
 
 entrada_11:
 	in temp,PB3
-	andi temp,0b00001100 ;verifica si est· en ceros
-	brne freq4 ;si est· en 4 salta a la frecuencia 4 
+	andi temp,0b00001100 ;verifica si est√° en ceros
+	brne freq4 ;si est√° en 4 salta a la frecuencia 4 
 	cbi PORTB,PB3
 
 freq1:
